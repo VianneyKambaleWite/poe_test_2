6014dd8c9be4a7bed9b9365df471b688c380f084
VianneyKambaleWite <129665185+VianneyKambaleWite@users.noreply.github.com>
2026-05-14 09:13:35 +0200

    Part2 created commit
    I  have created the part 2 Project

---
+++ Part_2_v2/src/main/java/com/mycompany/part_2_v2/Part_2_v2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.part_2_v2;
+
+/**
+ *
+ * @author wikambale
+ */
+public class Part_2_v2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
